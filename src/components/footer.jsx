fda06bc579e6ebe35001639993421eb4eda78c0a
Mcbelson32 <Mcbelson32@gmail.com>
2024-07-12 14:19:44 +0200

    rush again

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,20 +4,80 @@ import { Link } from "react-scroll";
 const Footer = () => {
   return (
     <footer className="contatiner bg-purple rounded-t-2xl mt-8 text-white">
-      <div className="flex justify-around items-center">
-        <Link
-          active={true}
-          activeClass="active"
-          spy={true}
-          offset={-100}
-          smooth={true}
-          duration={500}
-          to="home"
-        >
-          <h1 className="text-xl text-black font-semibold uppercase">
-            <span className="text-white">mc</span>bel.
-          </h1>
-        </Link>
+      <div className="w-full flex flex-wrap justify-around items-center pt-6 pb-4">
+        <div>
+          <Link
+            active={true}
+            spy={true}
+            offset={-100}
+            smooth={true}
+            duration={500}
+            to="home"
+          >
+            <h1 className="text-xl text-secondary font-semibold uppercase">
+              <span className="text-white">mc</span>bel.
+            </h1>
+          </Link>
+        </div>
+        <div>
+          <ul className="flex w-full flex-wrap items-center gap-x-10 text text-sm lg:gap-x-14 lg:text-base">
+            <li>
+              <Link
+                to="home"
+                spy={true}
+                offset={-80}
+                smooth={true}
+                duration={500}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                spy={true}
+                offset={-80}
+                smooth={true}
+                duration={500}
+                to="about"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                spy={true}
+                offset={-80}
+                smooth={true}
+                duration={500}
+                to="skills"
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                spy={true}
+                offset={-80}
+                smooth={true}
+                duration={500}
+                to="portfolio"
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link
+                spy={true}
+                offset={-80}
+                smooth={true}
+                duration={500}
+                to="contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <hr className="w-[90%] mx-auto bg-primary  " />
       <p className="down w-full text-xs text-center py-4">
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -10,9 +10,9 @@ const Contact = () => {
         Contact <span className="text-purple">Me</span>
       </h2>
       <p className="text-[13px] font-medium opacity-60">Get In Touch</p>
-      <div class="form-card1 mt-8 px-4 grid grid-cols-2 gap-8 items-center">
+      <div class="form-card1 mt-8 px-0 lg:px-4 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div className="col-span-1">lorem</div>
-        <div class="form-card2 col-span-1 px-5">
+        <div class="form-card2 col-span-1 lg:px-5">
           <form class="form" method="POST" className="w-full">
             <div class="form-field">
               <label htmlFor="name">Name</label>
